1fe15f78d34837817798c47163de432886f8fa27
Aris Goudouras <aris.gk@gmail.com>
2018-12-26 19:07:34 +0200

    Add songs service and repository unit tests

---
+++ data/songs/repository.test.js
@@ -0,0 +1,29 @@
+/* global it, describe */
+/* eslint-disable prefer-arrow-callback, func-names, no-unused-expressions */
+const { expect } = require('chai');
+const Song = require('../../domain/songs/model');
+const songsRepository = require('../../data/songs/repository');
+const parsedSongs = require('./songs.json');
+
+const songsRepo = songsRepository.create();
+
+const expected = parsedSongs.map(
+  song =>
+    new Song({
+      id: song.id,
+      title: song.title,
+      url: song.url,
+      artist: song.artist,
+      album: song.album,
+    }),
+);
+
+describe('songs JSON repository test', function() {
+  describe('list songs', function() {
+    it('should return an array of Song objects based on a JSON file', async function() {
+      const result = await songsRepo.listSongs();
+      expect(result).to.have.lengthOf(12);
+      expect(result).to.eql(expected);
+    });
+  });
+});
---
+++ domain/songs/service.test.js
@@ -0,0 +1,46 @@
+/* global it, describe, beforeEach, afterEach */
+/* eslint-disable prefer-arrow-callback, func-names, no-unused-expressions */
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Song = require('./model');
+const songsRepository = require('../../data/songs/repository');
+const songsService = require('./service');
+
+const songsRepo = songsRepository.create();
+const songsSvc = songsService.create(songsRepo);
+
+const songs = [
+  new Song({
+    id: 1,
+    title: 'Animals',
+    url: 'https://some-host.com/animals',
+    artist: 'Muse',
+    album: 'The 2nd Law',
+  }),
+  new Song({
+    id: 2,
+    title: 'Coming Back to Life',
+    url: 'https://some-host.com/coming-back-to-life',
+    artist: 'Pink Floyd',
+    album: 'The Division Bell',
+  }),
+];
+
+describe('songs service test', function() {
+  describe('list songs', function() {
+    beforeEach(() => {
+      sinon.stub(songsRepo, 'listSongs');
+    });
+
+    afterEach(() => {
+      songsRepo.listSongs.restore();
+    });
+
+    it('should call the repository to fetch the songs', async function() {
+      songsRepo.listSongs.resolves(songs);
+      const result = await songsSvc.listSongs();
+      expect(result).to.have.lengthOf(2);
+      expect(result).to.eql(songs);
+    });
+  });
+});
